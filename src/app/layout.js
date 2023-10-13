ccf8e3afb543b5cff3c387f1825958c30970e721
ElGibran17 <ibrahimelgibran17@gmail.com>
2023-10-13 20:46:28 +0700

    Update NotFound and Loading

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
-import { Inter } from "next/font/google";
+import { Gabarito } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const gabarito = Gabarito({ subsets: ["latin"] });
 
 export const metadata = {
   title: "BranAnimeList",
@@ -12,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body
+        className={`${gabarito.className} bg-color-dark`}
+        suppressHydrationWarning={true}
+      >
         <Navbar />
         {children}
       </body>
